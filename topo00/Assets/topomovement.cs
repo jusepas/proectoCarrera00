05d53e391548418861ed62a54be018a5c6476520
jusepas <julia.sevilla.2@gmail.com>
2015-05-02 19:08:44 +0200

    PrimeraPrueba
    Primera prueba: 2-05-15 Crear fondo, insertar topos  movimiento basico

---
+++ topo00/Assets/topomovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class topomovement : MonoBehaviour {
+	public Vector3 position;
+	public bool left = false;
+	public float velocity;
+
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position = position;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (position.x >= 5f) {
+			left = true;
+		}
+		if (position.x <= -5f) {
+			left = false;
+		}
+		if (left == true) {
+
+			position.x = position.x - velocity;
+		} 
+		else {
+			position.x = position.x + velocity;
+		}
+		gameObject.transform.position = position;
+	
+	}
+}
